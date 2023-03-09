72c0cb7cb3443a9232de2f5e3275374a5bdb3acf
WaShindeiru <fdata@student.agh.edu.pl>
2023-03-09 21:46:29 +0100

    second lab completed

--- lab2/doubly_linked_list.py
+++ lab2/doubly_linked_list.py
@@ -22,13 +22,30 @@ class DoublyLinkedList:
         if self._tail is None:
             self._tail = Node(data, None, None)
             self._head = self._tail
-        temp = Node(data, None, self._tail)
-        self._tail.next = temp
-        self._tail = temp
+        else:
+            temp = Node(data, None, self._tail)
+            self._tail.next = temp
+            self._tail = temp
 
     def remove(self):
-        self.head = self.head.next
-        self.head.prev = None
+        if self._head is None:
+            pass
+        elif self._head.next is None:
+            self._head = None
+            self._tail = None
+        else:
+            self._head = self._head.next
+            self._head.prev = None
+
+    def remove_end(self):
+        if self._tail is None:
+            pass
+        elif self._tail.prev is None:
+            self._head = None
+            self._tail = None
+        else:
+            self._tail = self._tail.prev
+            self._tail.next = None
 
     def length(self):
         if self._head is None:
@@ -57,9 +74,41 @@ class DoublyLinkedList:
         repr += str(temp.data)
         return repr
 
-example = DoublyLinkedList()
-example.add(1)
-example.append(10)
-example.add(5)
-example.append(55)
-print(example)
+    def get(self):
+        if self._head is None:
+            return None
+        return self._head.data
+
+    def destroy(self):
+        if self._head is None:
+            pass
+        temp = self._head
+        while(not temp.next is None):
+            temp = temp.next
+            temp.prev.next = None
+            temp.prev = None
+        self._head = None
+        self._tail = None
+
+def main():
+    uczelnie = DoublyLinkedList()
+    uczelnie.append(('AGH', 'Kraków', 1919))
+    uczelnie.append(('UJ', 'Kraków', 1364))
+    uczelnie.append(('PW', 'Warszawa', 1915))
+    uczelnie.add(('UW', 'Warszawa', 1915))
+    uczelnie.add(('UP', 'Poznań', 1919))
+    uczelnie.add(('PG', 'Gdańsk', 1945))
+
+    print(uczelnie)
+
+    print("Długość listy {}".format(uczelnie.length()))
+    uczelnie.remove()
+    print("Pierwszy element: {}".format(uczelnie.get()))
+    uczelnie.remove_end()
+    print(uczelnie)
+    uczelnie.destroy()
+    uczelnie.remove()
+    uczelnie.remove_end()
+
+if __name__ == "__main__":
+    main()
